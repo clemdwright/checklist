e280846807a93c8112a02b2f4d25a268bffa2710
Clem Wright <clemw@google.com>
2014-08-22 16:56:36 -0700

    began adding logic to check for play services, does not work yet

--- app/src/main/java/com/example/clemw/checklist/CheckForGooglePlayServices.java
+++ app/src/main/java/com/example/clemw/checklist/CheckForGooglePlayServices.java
@@ -6,106 +6,14 @@ import android.app.DialogFragment;
 import android.content.Intent;
 import android.os.Bundle;
 import android.util.Log;
-import android.view.Menu;
-import android.view.MenuItem;
-import android.view.View;
-import android.widget.AdapterView;
-import android.widget.ListView;
 
 import com.google.android.gms.common.ConnectionResult;
 import com.google.android.gms.common.GooglePlayServicesUtil;
 
-import org.json.JSONArray;
-import org.json.JSONObject;
-
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.List;
-
-
-public class MyActivity extends Activity {
-
-    private ListAdapter adapter;
-    private static final String url = "https://maps.googleapis.com/"
-            + "maps/api/place/nearbysearch/json?location=37.760107,-122.425908"
-            + "&radius=500&types=food&key=AIzaSyDSxGRYQXuA7qy3Rzcu1zILt2hAqbNcHaM";
-
-
-    @Override
-    protected void onCreate(Bundle savedInstanceState) {
-        super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_my);
-
-        ListView listView = (ListView) findViewById(R.id.listView);
-        adapter = new ListAdapter(this);
-        listView.setAdapter(adapter);
-
-        listView.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-
-            @Override
-            public void onItemClick(AdapterView<?> parent, View v, int position, long id) {
-                Log.i("MyActivity", "item clicked");
-                Intent intent = new Intent(MyActivity.this, DetailsActivity.class);
-                Place place = (Place) adapter.getItem(position);
-                String place_id = place.getPlaceId();
-                intent.putExtra("place_id", place_id);
-                startActivity(intent);
-            }
-        });
-    }
-
-    @Override
-    public boolean onCreateOptionsMenu(Menu menu) {
-        // Inflate the menu; this adds items to the action bar if it is present.
-        getMenuInflater().inflate(R.menu.my, menu);
-        return true;
-    }
-
-    @Override
-    protected void onStart() {
-        super.onStart();
-        JsonParser atomParser = new JsonParser(url);
-        atomParser.parse(new JsonParser.ParseCompleteCallback() {
-
-            public void onParseComplete(JSONObject jsonObject) {
-                List<Place> places = parsePlacesList(jsonObject);
-                adapter.setPlaces(places);
-                adapter.notifyDataSetChanged();
-            }
-        });
-    }
-
-    private List<Place> parsePlacesList(JSONObject jsonObject) {
-        List<Place> list = new ArrayList<Place>();
-        try {
-            JSONArray places = jsonObject.getJSONArray("results");
-            for (int i = 0; i < places.length(); i++) {
-                JSONObject place = places.getJSONObject(i);
-                Place item = new Place(place);
-                list.add(item);
-            }
-            return list;
-        } catch (Exception e) {
-            Log.e("JsonParser", "Error creating list from parsed JSON");
-            return Collections.emptyList();
-        }
-    }
-
-    @Override
-    public boolean onOptionsItemSelected(MenuItem item) {
-        // Handle action bar item clicks here. The action bar will
-        // automatically handle clicks on the Home/Up button, so long
-        // as you specify a parent activity in AndroidManifest.xml.
-        int id = item.getItemId();
-        if (id == R.id.action_settings) {
-            return true;
-        }
-        return super.onOptionsItemSelected(item);
-    }
-
-//    Adding real location
-//    http://developer.android.com/training/location/retrieve-current.html
-
+/**
+ * Created by clemw on 8/22/14.
+ */
+public class CheckForGooglePlayServices {
     /*
 * Define a request code to send to Google Play services
 * This code is returned in Activity.onActivityResult
@@ -157,9 +65,9 @@ public class MyActivity extends Activity {
                         break;
                 }
 
+            }
         }
     }
-}
 
     private boolean servicesConnected() {
         // Check that Google Play services is available
